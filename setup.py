0eea9499d55ed73f2bd42c1dc2b03ca1f23089c5
davux <da@weeno.net>
2010-12-06 18:24:33 -0600

    Make this look like a real package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "bitcoim",
+    version = "0.0.1",
+    description = "BitcoIM working module",
+    keywords = "bitcoin bitcoIM",
+    author = "David Ammouial",
+    url = "http://gitorious.org/bitcoim/python-bitcoim/",
+    license = "GPLv2",
+    long_description = """""",
+    packages = ['bitcoim']
+)

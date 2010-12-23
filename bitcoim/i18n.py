07c79a09ddb5b6fbe259854b7dc579803757d3d5
davux <da@weeno.net>
2010-12-22 22:05:38 -0600

    Internationalization module
    Provide a new module for handling internationalized message translation.
    This will allow to stop hard-coding English messages in the code.
    
    There are 2 main goals:
     - permit translation in other languages
     - provide a way for service administrators to customize the messages
       if they need to.
    
    This module is not used anywhere yet.

---
+++ bitcoim/i18n.py
@@ -0,0 +1,37 @@
+"""Internationalization module."""
+
+from ConfigParser import SafeConfigParser, NoSectionError, NoOptionError
+from os.path import join as j
+from os import sep
+from sys import prefix
+
+_paths = []
+for dir in [j(prefix, 'share'), j(prefix, 'local', 'share'), j(sep, 'etc')]:
+    _paths.append(j(dir, 'bitcoim', 'messages'))
+
+fallbackLangs = ['en']
+"""Fallback languages to try in turn, when a translation in a certain language
+didn't work.
+"""
+
+_parsers = {}
+
+def _(section, key, lang=None, fallback=list(fallbackLangs)):
+    '''Translate `key` (found in [`section`]) in `lang`. The translation is
+       looked up in the paths given by `paths`.
+       If the lookup fails, the translation is intented again with each
+       language given in the `fallback` list, whose default value is that of
+       `i18n.fallbackLangs`.
+    '''
+    if lang is None:
+        try:
+            lang = fallback.pop(0)
+        except IndexError:
+            return key
+    if not lang in _parsers:
+        _parsers[lang] = SafeConfigParser()
+        _parsers[lang].read(map(lambda p: j(p, lang), _paths))
+    try:
+        return _parsers[lang].get(section, key)
+    except NoSectionError, NoOptionError:
+        return _(section, key, None, fallback)
